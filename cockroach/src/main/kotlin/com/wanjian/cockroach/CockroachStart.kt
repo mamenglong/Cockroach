7e2fe15389c87302fc2f1dbf968845d5cdb4926a
mml <>
2023-02-09 17:15:37 +0800

    feat:注释

--- cockroach/src/main/kotlin/com/wanjian/cockroach/CockroachStart.kt
+++ cockroach/src/main/kotlin/com/wanjian/cockroach/CockroachStart.kt
@@ -19,18 +19,25 @@ object CockroachStart {
             DebugSafeModeUI.init(context as Application)
         }
         Cockroach.install(context,object : ExceptionHandler() {
+            /**
+             * 子线程和第一次主线程崩溃回调
+             */
             override fun onUncaughtExceptionHappened(thread: Thread?, throwable: Throwable?) {
                 //捕获监听中异常，防止使用方代码抛出异常时导致的反复调用
                 CockroachLog.loge("--->onUncaughtExceptionHappened:$thread<---",throwable)
                 CrashLog.saveCrashLog(context, throwable)
                 exceptionHandler?.uncaughtExceptionHappened(thread, throwable)
             }
-
+            /**
+             * 进入异常拦截后 又出现异常回调
+             */
             override fun onBandageExceptionHappened(throwable: Throwable?) {
                 CockroachLog.loge("--->onBandageExceptionHappened<---",throwable)
                 exceptionHandler?.bandageExceptionHappened(throwable)
             }
-
+            /**
+             * 主线程第一次崩溃会进入安全模式 即主动进入异常拦截
+             */
             override fun onEnterSafeMode() {
                 CockroachLog.loge("--->onEnterSafeMode<---")
                 exceptionHandler?.enterSafeMode()
@@ -43,10 +50,13 @@ object CockroachStart {
 
             }
 
+            /**
+             * ui绘制异常
+             */
             override fun onMayBeBlackScreen(e: Throwable?) {
                 super.onMayBeBlackScreen(e)
                 val thread = Looper.getMainLooper().thread
-                CockroachLog.loge("--->onUncaughtExceptionHappened:$thread<---", e)
+                CockroachLog.loge("--->onMayBeBlackScreen:$thread<---", e)
                 //黑屏时建议直接杀死app
                 exceptionHandler?.onMayBeBlackScreen(e)
             }
